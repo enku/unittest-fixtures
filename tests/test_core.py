856f8c997a0a1a882449c257db498896eb7acee3
Albert Hopkins <marduk@letterboxes.org>
2024-08-05 22:03:03 -0500

    add some more tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,41 @@
 # pylint: disable=missing-docstring
 from pathlib import Path
+from unittest import mock
 
 import unittest_fixtures as uf
 from tests import fixtures, fixtures1
+
+
+class GetResulTests(uf.BaseTestCase):
+    # pylint: disable=protected-access
+    class TestCase(uf.BaseTestCase):
+        pass
+
+    def test_when_given_generator_function(self) -> None:
+        return_value = object()
+        test = self.TestCase()
+        test._options = {}
+        test.fixtures = uf.Fixtures(name="test", foo="bar")
+        self.assertEqual(len(test._cleanups), 0)
+
+        def func(_options: uf.FixtureOptions, _fixtures: uf.Fixtures) -> object:
+            yield return_value
+
+        result = uf.get_result(func, test)
+
+        self.assertEqual(result, return_value)
+        self.assertEqual(len(test._cleanups), 1)
+
+    def test_calls_func_with_fixture_and_options(self) -> None:
+        return_value = object()
+        func = mock.Mock(spec=uf.FixtureFunction, return_value=return_value)
+        test = self.TestCase()
+        test._options = {}
+        test.fixtures = uf.Fixtures(name="test", foo="bar")
+
+        result = uf.get_result(func, test)
+        self.assertEqual(result, return_value)
+        func.assert_called_once_with(test._options, test.fixtures)
 
 
 @uf.requires("cd_to_tmpdir", "clear_cache")
