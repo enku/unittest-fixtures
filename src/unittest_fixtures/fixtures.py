d25aa508da2417e871569ffcb0db904b42803e51
Albert Hopkins <marduk@letterboxes.org>
2025-04-23 17:52:03 -0500

    linting

--- src/unittest_fixtures/fixtures.py
+++ src/unittest_fixtures/fixtures.py
@@ -26,7 +26,9 @@ _FIXTURES: dict[TestCase, Fixtures] = {}
 class TestMethodWithFixturesKwarg(Protocol):  # pylint: disable=too-few-public-methods
     """Test methods that take a fixtures kwarg"""
 
-    def __call__(self, _self: TestCase, *, fixtures: Fixtures) -> Any: ...  # pragma: no cover
+    def __call__(
+        self, _self: TestCase, *, fixtures: Fixtures
+    ) -> Any: ...  # pragma: no cover
 
 
 def given(
