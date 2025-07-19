5de8ac486e2698e295aea18a40798659a4ba6988
Albert Hopkins <marduk@letterboxes.org>
2025-07-19 18:11:25 -0500

    fixtures: distinguish btw TestCase instance and class
    Use variable names that better distinguish bettween TestCase instances
    and TestCase classes.

--- src/unittest_fixtures/fixtures.py
+++ src/unittest_fixtures/fixtures.py
@@ -37,24 +37,24 @@ def given(
 ) -> Callable[[TestCaseClass], TestCaseClass]:
     """Decorate the TestCase to include the fixtures given by the FixtureFunction"""
 
-    def decorator(test_case: TestCaseClass) -> TestCaseClass:
-        _state.requirements[test_case] = (
+    def decorator(test_class: TestCaseClass) -> TestCaseClass:
+        _state.requirements[test_class] = (
             {}
-            | ancestor_requirements(test_case)
+            | ancestor_requirements(test_class)
             | {funcname(f): f for req in requirements for f in [req]}
             | named_requirements
         )
 
-        for name, method in test_case.__dict__.items():
+        for name, method in test_class.__dict__.items():
             if callable(method) and (name == "test" or name.startswith("test")):
                 if not hasattr(method, "__unittest_fixtures_wrapped__"):
-                    setattr(test_case, name, make_wrapper(method))
+                    setattr(test_class, name, make_wrapper(method))
 
-        original_setup = getattr(test_case, "setUp", lambda *args, **kwargs: None)
+        original_setup = getattr(test_class, "setUp", lambda *args, **kwargs: None)
 
         def unittest_fixtures_setup(self: TestCase, *args: Any, **kwargs: Any) -> None:
             _state.fixtures[self] = Fixtures()
-            setups = _state.requirements.get(test_case, {})
+            setups = _state.requirements.get(test_class, {})
             add_fixtures(self, setups)
 
             if original_setup.__name__ != "unittest_fixtures_setup":
@@ -62,8 +62,8 @@ def given(
 
             self.addCleanup(lambda: _state.fixtures.pop(self, None))
 
-        setattr(test_case, "setUp", unittest_fixtures_setup)
-        return test_case
+        setattr(test_class, "setUp", unittest_fixtures_setup)
+        return test_class
 
     return decorator
 
@@ -84,10 +84,10 @@ def fixture(
 def where(**kwargs: Any) -> Callable[[TestCaseClass], TestCaseClass]:
     """Provide the given options to the given fixtures"""
 
-    def decorator(test_case: TestCaseClass) -> TestCaseClass:
-        test_case_options = _state.options.setdefault(test_case, {})
-        test_case_options.update(kwargs)
-        return test_case
+    def decorator(test_class: TestCaseClass) -> TestCaseClass:
+        test_class_options = _state.options.setdefault(test_class, {})
+        test_class_options.update(kwargs)
+        return test_class
 
     return decorator
 
@@ -115,10 +115,10 @@ def add_fixtures(test: TestCase, reqs: dict[str, FixtureFunction]) -> None:
             setattr(fixtures, name, apply_func(func, name, test))
 
 
-def ancestor_requirements(test_case: TestCaseClass) -> dict[str, FixtureFunction]:
-    """Gather the requirements of the test_case's ancestors"""
+def ancestor_requirements(test_class: TestCaseClass) -> dict[str, FixtureFunction]:
+    """Gather the requirements of the test_class's ancestors"""
     reqs = {}
-    for ancestor in reversed(test_case.mro()):
+    for ancestor in reversed(test_class.mro()):
         reqs.update(_state.requirements.get(ancestor, {}))
     return reqs
 
@@ -129,11 +129,11 @@ def apply_func(func: FixtureFunction, name: str, test: TestCase) -> Any:
     If func is a generator function, apply it and add it to the test's cleanup.
     """
     fixtures = copy(_state.fixtures[test])
-    test_case = type(test)
+    test_class = type(test)
     test_opts = {
         k: v
-        for test_case in (*reversed(test_case.mro()), test_case)
-        for k, v in _state.options.get(test_case, {}).items()
+        for test_class in (*reversed(test_class.mro()), test_class)
+        for k, v in _state.options.get(test_class, {}).items()
     }
     opts = opts_for_name(name, test_opts)
 
