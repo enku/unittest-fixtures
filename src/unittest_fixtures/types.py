445c3c6fee966663db20f3693654c1300dd5a069
Albert Hopkins <marduk@letterboxes.org>
2025-07-21 07:12:16 -0500

    types: covert Fixtures in to a (sub)class
    This is because error messages (e.g. attribute errors) display
    'SimpleNamespace' instead of 'Fixtures', which may be confusing to the
    user. It is preferred that the error messages display 'Fixtures'.

--- src/unittest_fixtures/types.py
+++ src/unittest_fixtures/types.py
@@ -4,7 +4,11 @@ from types import SimpleNamespace
 from typing import Any, Callable, Iterator, TypeAlias
 from unittest import TestCase
 
-Fixtures: TypeAlias = SimpleNamespace
+
+class Fixtures(SimpleNamespace):  # pylint: disable=too-few-public-methods
+    '''This is mainly so errors say "Fixtures" instead of "SimpleNamespace"'''
+
+
 FixtureContext: TypeAlias = Iterator
 FixtureFunction: TypeAlias = Callable[[Fixtures], Any]
 TestCaseClass: TypeAlias = type[TestCase]
