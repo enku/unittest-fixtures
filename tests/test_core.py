enku/unittest-fixtures

commit a018999175de6d54a960665e6b04581e225acfbd

apply_func: remove unnecessary parameter
We don't need to pass the fixture name to apply_func() as we're passing
the fixture function and the name can be derived from fixture_name().

src/unittest_fixtures/fixtures.py
@@ -103,9 +103,9 @@ class UnittestFixtures:
             if deps := self.state.deps.get(func, {}):
                 self.add_fixtures(test_case, deps)
             if not hasattr(fixtures, name):
-                setattr(fixtures, name, self.apply_func(func, name, test_case))
+                setattr(fixtures, name, self.apply_func(func, test_case))
 
-    def apply_func(self, func: FixtureFunction, name: str, test_case: TestCase) -> Any:
+    def apply_func(self, func: FixtureFunction, test_case: TestCase) -> Any:
         """Apply the given fixture func to the given test options and return the result
 
         If func is a generator function, apply it and add it to the test's cleanup.
@@ -117,7 +117,7 @@ class UnittestFixtures:
             for test_class in (*reversed(test_class.mro()), test_class)
             for k, v in self.state.options.get(test_class, {}).items()
         }
-        opts = opts_for_name(name, test_opts)
+        opts = opts_for_name(fixture_name(func), test_opts)
 
         if inspect.isgeneratorfunction(func):
             return test_case.enterContext(contextmanager(func)(fixtures, **opts))

commit e321a74ec15b291d2016f6eed80324fbea8f9a3e

fix regression using @where and named fixtures
Commit a018999 introduced a regression whereby fixtures having alternate
names and options passed in @where stopped working correctly.  An
example is the following (from the unit test):

```python
@dataclass
class Player:
    username: str

@fixture()
def player(_, username="test") -> Player:
    return Player(username=username)

@given(player, player2=player)
@where(player2__username="player2")
class MyTestCase(TestCase):
    def test(self) -> None:
        self.assertEqual(fixtures.player.username, "test")
        self.assertEqual(fixtures.player2.username, "player2")
```

For the above case commit a018999 caused the `player2` fixture not to
have the username "player2" but instead the default "test".

src/unittest_fixtures/fixtures.py
@@ -103,9 +103,9 @@ class UnittestFixtures:
             if deps := self.state.deps.get(func, {}):
                 self.add_fixtures(test_case, deps)
             if not hasattr(fixtures, name):
-                setattr(fixtures, name, self.apply_func(func, test_case))
+                setattr(fixtures, name, self.apply_func(func, name, test_case))
 
-    def apply_func(self, func: FixtureFunction, test_case: TestCase) -> Any:
+    def apply_func(self, func: FixtureFunction, name: str, test_case: TestCase) -> Any:
         """Apply the given fixture func to the given test options and return the result
 
         If func is a generator function, apply it and add it to the test's cleanup.
@@ -117,7 +117,7 @@ class UnittestFixtures:
             for test_class in (*reversed(test_class.mro()), test_class)
             for k, v in self.state.options.get(test_class, {}).items()
         }
-        opts = opts_for_name(fixture_name(func), test_opts)
+        opts = opts_for_name(name, test_opts)
 
         if inspect.isgeneratorfunction(func):
             return test_case.enterContext(contextmanager(func)(fixtures, **opts))

tests/test_core.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-docstring,protected-access
+from dataclasses import dataclass
 from unittest import IsolatedAsyncioTestCase, TestCase
 
 from unittest_fixtures import FixtureContext, Fixtures, fixture, given, where
@@ -139,6 +140,25 @@ class RequiresTests(TestCase):
         class MyTestCase(TestCase):
             def test(self, fixtures: Fixtures) -> None:
                 self.assertEqual("Hello World!", fixtures.echo)
+
+        result = MyTestCase("test").run()
+        assert_test_result(self, result)
+
+    def test_options_with_alternate_names(self) -> None:
+        @dataclass
+        class Player:
+            username: str
+
+        @fixture()
+        def player(_: Fixtures, username: str = "test") -> Player:
+            return Player(username=username)
+
+        @given(player, player2=player)
+        @where(player2__username="player2")
+        class MyTestCase(TestCase):
+            def test(self, fixtures: Fixtures) -> None:
+                self.assertEqual(fixtures.player.username, "test")
+                self.assertEqual(fixtures.player2.username, "player2")
 
         result = MyTestCase("test").run()
         assert_test_result(self, result)

tests/test_fixtures.py
@@ -218,7 +218,7 @@ class ApplyFuncTests(TestCase):
         uf.state.fixtures[t] = Fixtures()
         uf.state.options[Tests] = {"my": 7}
 
-        result = uf.apply_func(my_fixture, t)
+        result = uf.apply_func(my_fixture, "my", t)
 
         self.assertEqual(7, result)
 
@@ -240,7 +240,7 @@ class ApplyFuncTests(TestCase):
         t = Tests()
         uf.state.fixtures[t] = Fixtures()
 
-        uf.apply_func(my_fixture, t)
+        uf.apply_func(my_fixture, "my", t)
 
         self.assertTrue(in_context)
 