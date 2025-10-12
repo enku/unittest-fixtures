1e01bde34a9ee2d7a787b8329cea4eca8111ec11
Albert Hopkins <marduk@letterboxes.org>
2025-10-12 10:48:38 -0500

    parametrized: use parametrized generics syntax

--- src/unittest_fixtures/parametrized.py
+++ src/unittest_fixtures/parametrized.py
@@ -1,12 +1,11 @@
 """Parametrized subTest helper"""
 
 from functools import wraps
-from typing import Any, Callable, TypeAlias, TypeVar
+from typing import Any, Callable, TypeAlias
 from unittest import TestCase
 
 Param: TypeAlias = list[Any]
 Params: TypeAlias = list[Param]
-T = TypeVar("T", bound=TestCase)
 TestFunc: TypeAlias = Callable[..., Any]
 
 
@@ -15,7 +14,7 @@ def parametrized(lists_of_args: Params) -> Callable[[TestFunc], TestFunc]:
 
     def decorator(func: TestFunc) -> TestFunc:
         @wraps(func)
-        def wrapper(self: T, *args: Any, **kwargs: Any) -> None:
+        def wrapper[T: TestCase](self: T, *args: Any, **kwargs: Any) -> None:
             for list_of_args in lists_of_args:
                 name = ",".join(str(i) for i in list_of_args)
                 with self.subTest(name):
